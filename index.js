7dd84f6464fc46693e74a7aab1a921a1322a6621
Joshua Davis <me@jdav.is>
2017-06-07 00:18:42 -0400

    initial check-in

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')  
+const app = express()  
+const port = 8080
+
+app.get('/', (request, response) => {  
+  response.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
